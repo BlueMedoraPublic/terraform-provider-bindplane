7a8a6b2db483b6a42322bb55d630be6b62188f5e
jsirianni <jsirianni@localhost.localdomain>
2020-03-02 17:22:12 -0500

    resolves https://github.com/BlueMedoraPublic/bpcli/issues/36

--- provider/util/trim/trim.go
+++ provider/util/trim/trim.go
@@ -4,13 +4,15 @@ import (
 	"bytes"
 	"encoding/json"
 	"regexp"
+
+	"github.com/pkg/errors"
 )
 
 // Trim strips all whitespace and new lines frmo a json string
 func Trim(s string) (string, error) {
 	s, err := removeWhiteSpace(s)
 	if err != nil {
-		return "", err
+		return "", errors.Wrap(err, "time.Trim() failed to remove whitespace from '" + s + "'")
 	}
 
 	s = removeNewLine(s)
@@ -19,6 +21,10 @@ func Trim(s string) (string, error) {
 }
 
 func removeWhiteSpace(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+	
 	buffer := new(bytes.Buffer)
 	err := json.Compact(buffer, []byte(s))
 	if err != nil {
--- provider/util/trim/trim_test.go
+++ provider/util/trim/trim_test.go
@@ -27,3 +27,14 @@ func TestTrim(t *testing.T) {
 		t.Errorf("Expected the length of trimmed string to be two characters less than origonal string")
 	}
 }
+
+func TestTrimEmptyStr(t *testing.T) {
+	s, err := Trim("")
+	if err != nil {
+		t.Errorf("Expected Trim() to return an empty string when given a string of length zery, got an error: " + err.Error())
+	}
+
+	if s != "" {
+		t.Errorf("Expected Trim() to return an empty string when given an empty string, got: '" + s + "'")
+	}
+}
