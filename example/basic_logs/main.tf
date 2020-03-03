18642283bf45b205b50b15839939f2c9087da9e6
jsirianni <jsirianni@localhost.localdomain>
2020-03-02 22:52:16 -0500

    cleanup example code. add variable use to show interpolation syntax

--- example/basic_logs/main.tf
+++ example/basic_logs/main.tf
@@ -1,12 +1,22 @@
+/*
+
+bpcli logs source type parameters \
+  --source-type-id mysql
+
+*/
+variable "error_log_path" {
+  description = "example: you can use interpolation for configuraton params"
+  default = "/var/log/mysql/mysqld.log"
+}
+
 resource "bindplane_log_source" "mysql" {
   name = "mysql-terraform"
   source_type_id = "mysql"
   source_version = "2.0.0"
-
   configuration = <<CONFIGURATION
 {
   "mysql_error": true,
-  "error_log_path": "/var/log/mysql/mysqld.log",
+  "error_log_path": "${var.error_log_path}",
   "mysql_slow_query": true,
   "slow_query_log_path": "/var/log/mysql/slow.log",
   "mysql_general": false,
@@ -16,18 +26,20 @@ resource "bindplane_log_source" "mysql" {
 CONFIGURATION
 }
 
-// service account json is pulled from gcp secret manager
-// to avoid putting secrets in the repo
+/*
+
+bpcli logs destination type parameters \
+  --destination-type-id stackdriver
+
+*/
 resource "bindplane_log_destination" "stackdriver" {
+  name = "stackdriver-terraform"
+  destination_type_id = "stackdriver"
+  destination_version = "1.3.2"
   configuration = <<CONFIGURATION
 {
-"name": "stackdriver-terraform",
-"configuration": {
   "credentials": ${data.google_secret_manager_secret_version.bindplane_svc_act.secret_data},
   "location": "us-west1"
-},
-"destination_type_id": "stackdriver",
-"destination_version": "1.3.2"
 }
 CONFIGURATION
 
@@ -35,12 +47,9 @@ CONFIGURATION
 
 /*resource "bindplane_log_template" "mysql_prod" {
     name = "template-terraform"
-
     source_config_ids = [
         bindplane_log_source.mysql.id
     ]
-
     destination_config_id =  bindplane_log_destination.stackdriver.id
     agent_group = ""
-}
-*/
+}*/
