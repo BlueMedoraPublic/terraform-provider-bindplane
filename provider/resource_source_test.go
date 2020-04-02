27b215bfbd51c6c6795062e15a5d373e405f3cb8
Joseph Sirianni <joe.sirianni@bluemedora.com>
2020-04-02 01:41:44 -0400

    add fake uuid const for test cases

--- provider/resource_source_test.go
+++ provider/resource_source_test.go
@@ -9,7 +9,8 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-const fakeValidUUIDSRC = "abcdefAB-0123-4ABC-ab12-CDEF01234567"
+const fakeValidUUID = "abcdefAB-0123-4ABC-ab12-CDEF01234567"
+
 const fakeValidJSON = "{\"key\":\"value\"}" // important that there are no spaces
 
 func TestInitSource(t *testing.T) {
